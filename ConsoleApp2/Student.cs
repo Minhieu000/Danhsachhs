0cc7a488b41058a72cbb487f3bb3dcbc65d5e73c
THIS PC <THIS PC@LAPTOP-13UV3BRD>
2024-11-19 23:17:13 +0700

    Add project files.

---
+++ ConsoleApp2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Student
+    {
+        public int Id { get; set; }        // Mã số học sinh
+        public string Name { get; set; }  // Tên học sinh
+        public int Age { get; set; }      // Tuổi học sinh
+
+    }
+}
